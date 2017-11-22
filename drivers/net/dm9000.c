6240461d4b5df1ac4394de2579c132a427490826
CCCCCoder <179901678@qq.com>
2017-11-22 22:20:24 +0800

    增加dm9000驱动

--- drivers/net/dm9000.c
+++ drivers/net/dm9000.c
@@ -1559,8 +1559,8 @@ dm9000_init(void)
 #if defined(CONFIG_ARCH_S3C2410)
 	unsigned int oldval_bwscon = *(volatile unsigned int *)S3C2410_BWSCON;
 	unsigned int oldval_bankcon4 = *(volatile unsigned int *)S3C2410_BANKCON4;
-	*((volatile unsigned int *)S3C2410_BWSCON) = 
-		(oldval_bwscon&~(3<<16))|S3C2410_BWSCON_DW4_16|S3C2410_BWSCON_WS4 | S3C2410_BWSCON_ST4;
+	*((volatile unsigned int *)S3C2410_BWSCON) =
+	(oldval_bwscon&~(3<<16))|S3C2410_BWSCON_DW4_16 | S3C2410_BWSCON_WS4 | S3C2410_BWSCON_ST4;
 	*((volatile unsigned int *)S3C2410_BANKCON4) = 0x1f7c;
 #endif	
 	printk(KERN_INFO "%s Ethernet Driver, V%s\n", CARDNAME, DRV_VERSION);
