00dcc628af87b2c554b5792440b65eacf91c3503
CCCCCoder <179901678@qq.com>
2017-12-12 10:45:20 +0800

    增加按键驱动编译为模块

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make clean
+#make clean
 make 
 rm /tftpboot/uImage
-mkimage -n 'uImage' -A arm -O linux -T kernel -C none -a 0x30008000 -e 0x30008040 -d /home/airui/Desktop/linux/linux/arch/arm/boot/zImage /tftpboot/uImage
+mkimage -n 'uImage' -A arm -O linux -T kernel -C none -a 0x30008000 -e 0x30008040 -d /home/look/Desktop/linux/arch/arm/boot/zImage /tftpboot/uImage
